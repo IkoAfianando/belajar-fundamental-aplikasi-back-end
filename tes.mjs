a23c8a84995857045db67da2549eb536d664a1b7
IkoAfianando <ikoafianando123@gmail.com>
2022-01-22 21:03:03 +0700

    Push

--- tes.mjs
+++ tes.mjs
@@ -1,5 +1,12 @@
 class FizzBuzz {
  constructor(name) {
   this.name = name;
+  this.fizzBuzz = (data) => {
+   for(let i = 1; i <= data; i++) {
+    const data1 = i % 3 === 0 && i % 5 === 0;
+    const data2 = i % 3 === 0;
+    const data3 = i % 5 === 0;
+   }
+  }
  }
 }
