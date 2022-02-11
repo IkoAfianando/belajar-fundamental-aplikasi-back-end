096fa68f7a0dbcb6f15c8de3b10a97630bf56085
IkoAfianando <ikoafianando123@gmail.com>
2022-02-11 21:45:19 +0700

    Push

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ class FizzBuzz {
                 console.log(`Fizz ${i}`);
             }else if(data3) {
                 console.log(`Buzz ${i}`);
+            }else {
+                console.log(i);
             }
         }
     }
