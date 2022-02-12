f79f7846c6b94b8c810a81b5a230068145f9c7b7
IkoAfianando <ikoafianando123@gmail.com>
2022-02-12 14:05:23 +0700

    Push

--- permutation.js
+++ permutation.js
@@ -3,13 +3,23 @@ function permutation(str) {
     return [str];
   }
   const result = [];
+  debugger;
   for (let i = 0; i < str.length; i++) {
+    debugger;
     const char = str[i];
+    debugger;
     const rest = str.slice(0, i) + str.slice(i + 1);
+    debugger;
     const perms = permutation(rest);
+    debugger;
     for (let j = 0; j < perms.length; j++) {
+      debugger;
       result.push(char + perms[j]);
+      debugger;
     }
   }
   return result;
 }
+
+console.log(permutation("abc"));
+
